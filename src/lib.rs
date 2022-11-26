093597f1e7d7a6d6a94fbb5d343552d5e5493309
Na'aman Hirschfeld <nhirschfeld@gmail.com>
2022-11-26 19:58:02 +0100

    (chore): add test

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,10 @@
 use urlencoding::decode_binary;
 
-pub fn parse_qsl(qs: &[u8]) -> Box<[(String, String)]> {
+pub fn parse_qsl(qs: &[u8], separator: char) -> Box<[(String, String)]> {
     String::from_utf8(decode_binary(qs).into_owned())
         .unwrap()
-        .split('&')
-        .map(|value| value.split_once('='))
-        .map(|value| (value.unwrap_or(("", ""))))
+        .split(separator)
+        .map(|value| value.split_once('=').unwrap_or(("", "")))
         .map(|value| (value.0.to_string(), value.1.to_string()))
         .collect::<Vec<(String, String)>>()
         .into_boxed_slice()
@@ -16,8 +15,23 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_qsl() {
-        let result = parse_qsl("key=1&key=2&anotherKey=a&yetAnother=z".as_bytes());
+    fn test_ampersand_separator() {
+        let result = parse_qsl("key=1&key=2&anotherKey=a&yetAnother=z".as_bytes(), '&');
+        assert_eq!(
+            result,
+            vec![
+                ("key".to_string(), "1".to_string()),
+                ("key".to_string(), "2".to_string()),
+                ("anotherKey".to_string(), "a".to_string()),
+                ("yetAnother".to_string(), "z".to_string())
+            ]
+            .into_boxed_slice()
+        );
+    }
+
+    #[test]
+    fn test_semicolon_separator() {
+        let result = parse_qsl("key=1;key=2;anotherKey=a;yetAnother=z".as_bytes(), ';');
         assert_eq!(
             result,
             vec![
