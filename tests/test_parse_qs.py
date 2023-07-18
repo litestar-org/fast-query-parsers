3567570a71c9b4367a4f94d5b1f87d619e500bc0
Na'aman Hirschfeld <nhirschfeld@gmail.com>
2023-07-18 15:52:53 +0200

    chore(tests): update QS tests

--- tests/test_parse_qs.py
+++ tests/test_parse_qs.py
@@ -4,21 +4,19 @@ from fast_query_parsers import parse_url_encoded_dict
 
 
 def test_parse_urlencoded_with_parse_numbers() -> None:
-    result = parse_url_encoded_dict(
-        urlencode(
-            [
-                ("value", "10"),
-                ("value", "12"),
-                ("veggies", '["tomato", "potato", "aubergine"]'),
-                ("nested", '{"some_key": "some_value"}'),
-                ("calories", "122.53"),
-                ("healthy", "true"),
-                ("polluting", "false"),
-                ("json", "null"),
-            ],
-            True,
-        ).encode()
-    )
+    encoded = urlencode(
+        [
+            ("value", "10"),
+            ("value", "12"),
+            ("veggies", '["tomato", "potato", "aubergine"]'),
+            ("nested", '{"some_key": "some_value"}'),
+            ("calories", "122.53"),
+            ("healthy", "true"),
+            ("polluting", "false"),
+            ("json", "null"),
+        ],
+    ).encode()
+    result = parse_url_encoded_dict(encoded, True)
     assert result == {
         "value": [10, 12],
         "veggies": ["tomato", "potato", "aubergine"],
@@ -31,21 +29,19 @@ def test_parse_urlencoded_with_parse_numbers() -> None:
 
 
 def test_parse_urlencoded_without_parse_numbers() -> None:
-    result = parse_url_encoded_dict(
-        urlencode(
-            [
-                ("value", "10"),
-                ("value", "12"),
-                ("veggies", '["tomato", "potato", "aubergine"]'),
-                ("nested", '{"some_key": "some_value"}'),
-                ("calories", "122.53"),
-                ("healthy", "true"),
-                ("polluting", "false"),
-                ("json", "null"),
-            ],
-            False,
-        ).encode()
-    )
+    encoded = urlencode(
+        [
+            ("value", "10"),
+            ("value", "12"),
+            ("veggies", '["tomato", "potato", "aubergine"]'),
+            ("nested", '{"some_key": "some_value"}'),
+            ("calories", "122.53"),
+            ("healthy", "true"),
+            ("polluting", "false"),
+            ("json", "null"),
+        ],
+    ).encode()
+    result = parse_url_encoded_dict(encoded, False)
     assert result == {
         "value": ["10", "12"],
         "veggies": ["tomato", "potato", "aubergine"],
