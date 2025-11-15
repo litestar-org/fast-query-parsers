e2f76868ebb32d780879cc41702dd4b71bae5702
winstxnhdw <winstxnhdw@gmail.com>
2025-11-15 18:01:02 +0800

    build: use latest `pyo3` API

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,7 @@
 mod query_string;
 
 use pyo3::prelude::*;
+use pyo3::types::PyAny;
 use pythonize::pythonize;
 
 pub use query_string::{parse_query_string as parse_qs, parse_query_string_to_json};
@@ -8,21 +9,24 @@ pub use query_string::{parse_query_string as parse_qs, parse_query_string_to_jso
 // parse query string into a list of tuples.
 #[pyfunction]
 #[pyo3(text_signature = "(qs, separator)")]
-fn parse_query_string(qs: &[u8], separator: char) -> PyResult<Vec<(String, String)>> {
-    Ok(parse_qs(qs, separator))
+fn parse_query_string(qs: &[u8], separator: char) -> Vec<(String, String)> {
+    parse_qs(qs, separator)
 }
 
 // parse query string into a python object.
 #[pyfunction]
-#[pyo3(signature = (qs, parse_numbers=true),text_signature = "(qs, parse_numbers=true)")]
-fn parse_url_encoded_dict(py: Python, qs: &[u8], parse_numbers: bool) -> PyResult<PyObject> {
-    Ok(pythonize(py, &parse_query_string_to_json(qs, parse_numbers)).unwrap())
+#[pyo3(signature = (qs, parse_numbers=true), text_signature = "(qs, parse_numbers=true)")]
+fn parse_url_encoded_dict(py: Python, qs: &[u8], parse_numbers: bool) -> Py<PyAny> {
+    pythonize(py, &parse_query_string_to_json(qs, parse_numbers))
+        .unwrap()
+        .into()
 }
 
-#[pymodule]
-fn fast_query_parsers(_py: Python<'_>, m: &PyModule) -> PyResult<()> {
-    m.add_function(wrap_pyfunction!(parse_query_string, m)?)?;
-    m.add_function(wrap_pyfunction!(parse_url_encoded_dict, m)?)?;
+#[pyo3::prelude::pymodule(gil_used = false)]
+mod fast_query_parsers {
+    #[pymodule_export]
+    use super::parse_query_string;
 
-    Ok(())
+    #[pymodule_export]
+    use super::parse_url_encoded_dict;
 }
